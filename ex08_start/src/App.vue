4811fdfe9acf718e3d05b13e9551d1eac8701023
codedesign-webapp <code.design.webapp@gmail.com>
2022-03-22 17:53:49 +0900

    update source

--- ex08_start/src/App.vue
+++ ex08_start/src/App.vue
@@ -1,23 +1,45 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
       <v-spacer></v-spacer>
+
       <v-btn
-        text
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
+        text
       >
         <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-content>
+    <v-main>
       <HelloWorld/>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
@@ -26,9 +48,11 @@ import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
     HelloWorld,
   },
+
   data: () => ({
     //
   }),
