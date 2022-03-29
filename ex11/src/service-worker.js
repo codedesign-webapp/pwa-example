69e03e3318eb19eff9ea714c2d6092db60140877
CODE*DESIGN 웹앱 <code.design.webapp@gmail.com>
2022-03-29 18:12:21 +0900

    update source

--- ex11/src/service-worker.js
+++ ex11/src/service-worker.js
@@ -1,18 +1,20 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');  // <-- workbox 최신번전 CDN 추가
+
 // Workbox를 디버그모드로 설정 
 workbox.setConfig({
   debug: true,
 });
 
 // Vue-Cli에서 기본으로 제공하는 프리캐시 설정을 Workbox에 적용 
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);  // <-- self.__WB_MANIFEST로 변경
 
 // 촬영된 이미지 캐시
 workbox.routing.registerRoute(
   new RegExp('https://firebasestorage.googleapis.com/v0/b/pwa-camera.appspot.com/.*'),
-  workbox.strategies.staleWhileRevalidate({
+  new workbox.strategies.StaleWhileRevalidate({
       cacheName: 'camera-images',
       plugins: [
-          new workbox.expiration.Plugin({
+          new workbox.expiration.ExpirationPlugin ({   // <-- ExpirationPlugin로 대문자 'E' 변경
             maxEntries: 60,
             maxAgeSeconds: 365 * 24 * 60 * 60, // 1년 지정
           }),
